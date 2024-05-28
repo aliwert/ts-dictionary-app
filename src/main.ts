526d0aec54ae2bc25f2cb032ce2d4135adee087c
aliwert <154356044+aliwert@users.noreply.github.com>
2024-05-28 13:26:03 +0300

    selected necessary elements from dom

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+// Selecting necessary elements from the DOM
+const container: HTMLElement = document.querySelector(".container")!;
+const searchInput: HTMLInputElement = container.querySelector("input")!;
+const volume: HTMLElement = container.querySelector(".word i")!;
+const infoText: HTMLElement = container.querySelector(".info-text")!;
+const synonyms: HTMLElement = container.querySelector(".synonyms .list")!;
+const removeIcon: HTMLElement = container.querySelector(".search span")!;
